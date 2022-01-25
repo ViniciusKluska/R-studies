b5c7703dabd855a540a24649b5e60d98f7502144
Vinicius Araujo Kluska <vinicius@kluska.com.br>
2022-01-25 17:25:04 -0300

    QQ-plot_Exercises.R Done

--- HarvardX_PH525_1x/Introduction_to_Exploratory_Data_Analysis/QQ-plot_Exercises.R
+++ HarvardX_PH525_1x/Introduction_to_Exploratory_Data_Analysis/QQ-plot_Exercises.R
@@ -5,15 +5,23 @@
         url <- "http://courses.edx.org/c4x/HarvardX/PH525.1x/asset/skew.RData"
         filename <- "skew.RData"
         destfile <- "C:/Users/klusk/Projects/R-studies/HarvardX_PH525_1x/Data_HarvardX/skew.RData"
-        download(url, destfile)
+        #download(url, destfile) (remove "#" to download the file)
 
     # 2. Load the data file into R using the following command: dat <- load("C:/Users/klusk/Projects/R-studies/HarvardX_PH525_1x/Data_HarvardX/skew.RData")
         
         load("C:/Users/klusk/Projects/R-studies/HarvardX_PH525_1x/Data_HarvardX/skew.RData")
-        dim(dat) -> Data_Base
-        par(mfrow = c(3,3)) -> Data_Base_1
-        for (i in 1:9) {qqnorm(dat[i])} -> Data
-        par(mfrow=c(1,1))
+        dim(dat) 
 
-print(Data_Base)
-print(Data_Base_1)
+    # 3. Create a data frame called "skew" using the following command: skew <- data.frame(dat)
+
+        skew <- data.frame(dat)
+        dim(skew)
+
+print(dim(skew))
+
+    # 4. for loop to calculate the skewness of each column in the data frame.
+
+        par(mfrow = c(3,3))
+        for (i in 1:9) {
+            qqnorm(skew[,i], main = paste("Skewness of column ", i, ": ", skew$names[i]), xlab = "Skewness", ylab = "Frequency")
+        }
